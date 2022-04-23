ff5d529f30973ad03e3a5031dfe3f03a8cc69b59
Katie <42517994+KConlonCoder@users.noreply.github.com>
2022-04-23 19:41:53 -0400

    Add files via upload
    Minimum Viable Product draft v1, initial design completed & mobile responsible

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+<<<<<<< HEAD
+/* Set the width of the side navigation to 250px */
+document.getElementById("menu-icon").addEventListener('click', openNav)
 
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+=======
+
+>>>>>>> d7605aed9b473ac6f47abbe1ebe37cbe7a84fac6
