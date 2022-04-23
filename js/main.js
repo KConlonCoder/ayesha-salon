c81e7296be2925e9e85096a9b789ee5bd3031cf4
Katie <42517994+KConlonCoder@users.noreply.github.com>
2022-04-23 19:49:18 -0400

    Add files via upload
    Previous files had merge conflict failures - recommitting

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* Set the width of the side navigation to 250px */
 document.getElementById("menu-icon").addEventListener('click', openNav)
 
@@ -10,6 +9,3 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
-=======
-
->>>>>>> d7605aed9b473ac6f47abbe1ebe37cbe7a84fac6
